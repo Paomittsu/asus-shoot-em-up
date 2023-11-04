7f0665629cf27cc0857346a08fd5e4a9284e0c8e
Paomittsu <philip.figueroa@obf.ateneo.edu>
2023-11-05 07:00:17 +0800

    Random Enemy spawning

---
+++ ASUS_ShootEmUp/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    [SerializeField] private float  spawnRate = 1f;
+
+    [SerializeField] private GameObject[] enemyPrefabs;
+
+    [SerializeField] private bool canSpawn = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Spawner());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator Spawner()
+    {
+        WaitForSeconds wait = new WaitForSeconds(spawnRate);
+        yield return wait;  
+        Instantiate(enemyPrefabs[0], transform.position, Quaternion.identity);
+        Debug.Log(transform.position);
+        
+    }
+}
